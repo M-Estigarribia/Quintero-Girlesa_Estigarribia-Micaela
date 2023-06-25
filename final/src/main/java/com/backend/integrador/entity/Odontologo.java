cf67b3d17daf2fa8c06f2819f697abafa6181a9a
Girlesa Quintero R <girlesa.quinteror@LATAM-XP308J.local>
2023-06-24 21:04:54 -0500

    refactor Odontologo entity

--- final/src/main/java/com/backend/integrador/entity/Odontologo.java
+++ final/src/main/java/com/backend/integrador/entity/Odontologo.java
@@ -1,41 +1,53 @@
 package com.backend.integrador.entity;
 
-public class Odontologo {
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-    private int id;
-    private int matricula;
+@Entity
+@Table(name = "ODONTOLOGOS")
+public class Odontologo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "La matricula no puede ser nula")
+    @NotBlank(message = "Debe especificarse la matricula del odontologo")
+    @Pattern(regexp = "^[A-Z]{2}-\\d{1,3}\\d*$")
+    @Size(min = 10, message = "El campo debe tener mínimo 10 caracteres")
+    private String matricula;
+
+    @Size(max = 50, message = "El nombre debe tener hasta 50 caracteres")
+    @NotNull(message = "El nombre no puede ser nulo")
+    @NotBlank(message = "Debe especificarse el nombre del odontologo")
     private String nombre;
+
+    @Size(max = 50, message = "El apellido debe tener hasta 50 caracteres")
+    @NotNull(message = "El apellido no puede ser nulo")
+    @NotBlank(message = "Debe especificarse el apellido del odontologo")
     private String apellido;
 
-    public Odontologo(){
+    public Odontologo() {
     }
 
-    public Odontologo(int id, int matricula, String nombre, String apellido) {
-        this.id = id;
+
+    public Odontologo(String matricula, String nombre, String apellido) {
         this.matricula = matricula;
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
-    public Odontologo(int matricula, String nombre, String apellido) {
-        this.matricula = matricula;
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getMatricula() {
+    public String getMatricula() {
         return matricula;
     }
 
-    public void setMatricula(int matricula) {
+    public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 
@@ -53,10 +65,5 @@ public class Odontologo {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
-    }
-
-    @Override
-    public String toString() {
-        return "\n Odontólogo Id: " + id + "\n   Dr/a: " + nombre + " " + apellido + "\n   Matrícula: " + matricula;
     }
 }
