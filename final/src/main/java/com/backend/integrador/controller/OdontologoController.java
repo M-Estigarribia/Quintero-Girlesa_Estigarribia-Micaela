9a366e4cbe15d1fab41c6d86337d2b9932dceaad
Girlesa Quintero R <girlesa.quinteror@LATAM-XP308J.local>
2023-06-24 21:26:36 -0500

    agregando controller

---
+++ final/src/main/java/com/backend/integrador/controller/OdontologoController.java
@@ -0,0 +1,26 @@
+package com.backend.integrador.controller;
+
+import com.backend.integrador.dto.OdontologoDto;
+import com.backend.integrador.entity.Odontologo;
+import com.backend.integrador.exceptions.ResourceNotFoundException;
+import com.backend.integrador.service.impl.OdontologoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/odontologos")
+public class OdontologoController {
+
+    private final OdontologoService odontologoService;
+
+    @Autowired
+    public OdontologoController(OdontologoService odontologoService) {
+        this.odontologoService = odontologoService;
+    }
+
+    // Necesitamos poner los endpoints aquí?
+}
